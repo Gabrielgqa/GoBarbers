7c9896af44d73848d40a97c08efdc1c191d60210
GabrielGQA <gabrielgqa@gmail.com>
2019-09-12 12:51:19 -0300

    Configurando nodemail e email de cancelamento de agendamento

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # GoBarber
 
-API para gestão de serviços de Barbearia. (express, jwt, sequelize, postgres)
+API para gestão de serviços de Barbearia. (express, jwt, sequelize, postgres, mongodb, nodemailer)
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '66c178f4a8ccae',
+    pass: '163cea39ecde64',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
